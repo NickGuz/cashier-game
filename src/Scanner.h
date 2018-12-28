2cb96b3e0e59c180ff19bb552dfae34adff431f4
NickGuz <niickguz@gmail.com>
2018-12-28 00:42:47 -0500

    First future commit & implement incredibly buggy state machine

---
+++ src/Scanner.h
@@ -0,0 +1,31 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#include "Texture.h"
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+class Scanner : public Texture
+{
+    public:
+        Scanner();
+        ~Scanner();
+        void load(SDL_Renderer* renderer);
+        void update();
+        void render();
+        SDL_Rect getCollider();
+        bool scanned;
+        int scannedFrames;
+
+    private:
+        Texture scanDefault;
+        Texture scanScanned;
+        int scanHeight, scanWidth;
+
+        SDL_Rect collider;
+
+        SDL_Renderer* mRenderer;
+};
+
+#endif
